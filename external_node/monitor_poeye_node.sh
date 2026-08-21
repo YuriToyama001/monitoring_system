bcf5d69e1cc8d837533f1ee5934cf5735d514766
touyama@networkdebug <yuuri.touyama.okamura@gmail.com>
2026-08-21 14:20:50 +0900

    POEYEストリーミングユニット追加

--- external_node/monitor_poeye_node.sh
+++ external_node/monitor_poeye_node.sh
@@ -7,24 +7,34 @@ set -euo pipefail
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 
 # 外部ノードのホスト名。monitor_all.conf で設定可能
-HOST=${POEYE_HOST:-}
+STRM_SERVER_HOST=${STREAMING_SERVER_HOST:-}
+STRM_UNIT_HOST=${STREAMING_UNIT_HOST:-}
 
-main() {
-    if ! ping -c 1 -W 1 "${HOST}" >/dev/null
+echo STRM_SERVER_HOST=${STRM_SERVER_HOST}
+echo STRM_UNIT_HOST=${STRM_UNIT_HOST}
+
+notify_and_log() {
+    local status="$1"
+    local message="$2"
+
+    "${SCRIPT_DIR}/../notify/notify_dispatch.sh" PO-EYE "${status}" "${message}"
+    "${SCRIPT_DIR}/../log_output/log_output_dispatch.sh" PO-EYE "${status}" "${message}"
+}
+
+ping_check() {
+    local host=$1
+    if ! ping -c 1 -W 1 "${host}" >/dev/null
     then
-        "${SCRIPT_DIR}/../notify/notify_dispatch.sh" PO-EYE ERROR "PING_FAIL:${HOST}"
-        "${SCRIPT_DIR}/../log_output/log_output_dispatch.sh" PO-EYE ERROR "PING_FAIL:${HOST}"
+        notify_and_log "ERROR" "PING_FAIL HOST=${host}"
         exit 1
     fi
 
-    # if ! curl -fs http://${HOST}:8080/health >/dev/null
-    # then
-    #     ${SCRIPT_DIR}/../notify/notify_dispatch.sh PO-EYE ERROR "SERVICE_FAIL"
-    #     exit 1
-    # fi
+    notify_and_log "OK" "PING_OK HOST=${host}"
+}
 
-    "${SCRIPT_DIR}/../notify/notify_dispatch.sh" PO-EYE OK "PING_OK:${HOST}"
-    "${SCRIPT_DIR}/../log_output/log_output_dispatch.sh" PO-EYE OK "PING_OK:${HOST}"
+main() {
+    ping_check "${STRM_SERVER_HOST}"
+    ping_check "${STRM_UNIT_HOST}"
 }
 
 main
